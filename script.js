8514671116e6d0ede257b4d4ebc330ec0f1da2a6
ko-23-047-sketch <ko-23-047@turan-edu.kz>
2026-04-09 19:18:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getStats() {
     let tag = document.getElementById('playerTag').value.replace('#', '');
     const proxy = "https://corsproxy.io/?"; // Используем прокси для обхода CORS
     const url = `https://api.brawlstars.com/v1/players/%23${tag}`;
-    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImQzYTc1M2QxLTBmMjUtNGI1ZS1iZDZlLTc2MTU2MGEyZDAwNSIsImlhdCI6MTc3NTc0MzE5NCwic3ViIjoiZGV2ZWxvcGVyLzcyZTYzYzc2LTc2NmEtZjkzNC02NzlhLTFmNWMwZTA4NTNhYyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMC4wLjAuMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.XQdQYCD2Fqd8ABVBE5Y4xsK7XImjTWsoNulOXXlpqiFXJNl7x9Db7hgMcgSxR36dhSM5FFDOVdEPCrvmMWxCZQ";
+    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjdhYzE4M2VmLTY3ZGUtNDYzNy04M2VhLTRmY2I0M2VhYjM3YSIsImlhdCI6MTc3NTc0NDIyMywic3ViIjoiZGV2ZWxvcGVyLzcyZTYzYzc2LTc2NmEtZjkzNC02NzlhLTFmNWMwZTA4NTNhYyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMC4wLjAuMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.nEV1awC1zPYxr56XrgsbrKsCxyaGnvkKmkqoChiTRIvQcx2Q-qkfWmYC13h3mx21xxPwll2j0XcrwzX42LI0LQ";
 
     try {
         const response = await fetch(proxy + encodeURIComponent(url), {
